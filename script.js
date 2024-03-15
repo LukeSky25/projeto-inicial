4164bf1c1877bf38a4ff8c2129ff34c291dee78b
LuceSky25 <lucasskywalkerodrigues@gmail.com>
2024-03-15 08:24:11 -0300

    Variavel Nome

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var botao = document.getElementById('botao');
-
+var nome  = document.getElementById('nome')
 
 botao.addEventListener('click', function(e) {
     
-    alert('Você Apertou o Botão ', e);
+    alert('Você Apertou o Botão ' + nome.value);
 });
