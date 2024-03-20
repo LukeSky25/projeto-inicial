10c879b068eaa0515e960cdad5a3b10e083abe75
LuceSky25 <lucasskywalkerodrigues@gmail.com>
2024-03-20 09:54:02 -0300

    Formulário

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var botao = document.getElementById('botao');
-var nome  = document.getElementById('nome')
+var saida = document.getElementById('saida-de-dados');
+var nome  = document.getElementById('nome');
+var email  = document.getElementById('email');
+var tel  = document.getElementById('tel');
+var cep = document.getElementById('cep');
+var logr = document.getElementById('logr');
+var num = document.getElementById('num');
+var compl = document.getElementById('compl');
+var bairro = document.getElementById('bairro');
+var cid = document.getElementById('cid');
+var estd = document.getElementById('estd');
+
 
 botao.addEventListener('click', function(e) {
     
-    alert('Você Apertou o Botão ' + nome.value);
+    saida.innerText = `Nome: ${nome.value}  ` + `\nEmail: ${email.value}  ` + `\nTelefone: ${tel.value}  ` + `\nCEP: ${cep.value}  ` + `\nLogradouro: ${logr.value}  ` + 
+    `\nNúmero: ${num.value}` + `\nComplemento: ${compl.value}  ` + `\nBairro: ${bairro.value}  ` + `\nCidade: ${cid.value}` +
+    `\nEstado: ${estd.value}`;
+
 });
